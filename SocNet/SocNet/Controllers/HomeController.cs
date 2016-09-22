90e9de68bb473ddd997df0b5bac2b932a08a5d6b
Student 212927 <212927@student.pwr.edu.pl>
2016-09-22 14:10:48 +0200

    W komentarzu przykładowe wywołanie bazy danych

--- SocNet/SocNet/Controllers/HomeController.cs
+++ SocNet/SocNet/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SocNet.Models;
 
 namespace SocNet.Controllers
 {
     public class HomeController : Controller
     {
+        //private NetworkDb ada =new NetworkDb();
+        //private LinkDb lin = new LinkDb();
         public ActionResult Index()
         {
             return View();
