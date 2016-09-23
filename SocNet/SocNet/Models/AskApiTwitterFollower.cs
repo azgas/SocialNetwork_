2898eeec147013b8490056296f5ca81e72ecaf19
Student 212927 <212927@student.pwr.edu.pl>
2016-09-23 09:49:27 +0200

    Dodanie modelów dla Formularzy zapytań do Api o sieci dla Twitter i Flickr

---
+++ SocNet/SocNet/Models/AskApiTwitterFollower.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocNet.Models
+{
+    public class AskApiTwitterFollower
+    {
+        public int networkID { get; set; }
+        public int initialVertex { get; set; }
+        public int queryLimit { get; set; }
+    }
+}
---
+++ SocNet/SocNet/Models/AskApiTwitterFollowers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocNet.Models
+{
+    public class AskApiTwitterFollowers
+    {
+        public int networkID { get; set; }
+        public int initialVertex { get; set; }
+        public int queryLimit { get; set; }
+        public int numberOfQueries { get; set; }
+    }
+}
