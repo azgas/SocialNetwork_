f06d8937dfa838e738244d9788e7937a6005bd00
azgas <azgaslik@gmail.com>
2016-09-23 09:36:12 +0200

    Dodany kontroler wizualizacji

---
+++ SocNet/SocNet/Controllers/VisualisationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocNet.Controllers
+{
+    public class VisualisationController : Controller
+    {
+        // GET: Visualisation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
