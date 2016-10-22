e000d0222e1718d292a49503f94356bd5803e0b0
LenroK <kornel133@gmail.com>
2016-10-22 19:06:40 +0200

    utworzenie odpowiednich usług w Flickr, utworzenie pierwszej usługi  Twitter, brakuje 4 i podłączenia do bazy danych

--- SocNet/FlickrServices/Controllers/FlickrFollowersAPIController.cs
+++ SocNet/FlickrServices/Controllers/FlickrFollowersAPIController.cs
@@ -1,4 +1,5 @@
-﻿using FlickrServices.Models;
+﻿using FlickrNet;
+using FlickrServices.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,185 @@ namespace FlickrServices.Controllers
     public class FlickrFollowersAPIController : ApiController
     {
         //POST: /api/FlickrFollowersAPI/FlickrFollowers
+        public void IsUserIdInVertex(string VertexId, int serviceID, string VertexName = null)
+        {
+            var existUser = network.VertexDb.Where<VertexDb>(x => x.identifier == VertexId && x.service_id == serviceID).ToList();
+            if (existUser.Count == 0 && VertexName != null)
+            {
+                var vertex = new VertexDb();
+                vertex.identifier = VertexId;
+                vertex.service_id = serviceID;
+                vertex.name = VertexName;
+                network.VertexDb.Add(vertex);
+                network.SaveChanges();
+            }
+            else if (existUser.Count == 0 && VertexName == null)
+            {
+                var vertex = new VertexDb();
+                vertex.identifier = VertexId;
+                vertex.service_id = serviceID;
+                network.VertexDb.Add(vertex);
+                network.SaveChanges();
+            }
+        }
         [HttpPost]
         public IHttpActionResult FlickrFollowers([FromBody] FormFlickrFollowers form)
         {
+            //data rozpoczęcia pobierania
+            var data = DateTime.Now;
+            //dodac pobieranie id serwisu z bazy danych
+            var serviceObj = network.ServiceDb.Where<ServiceDb>(x => x.name == "Flickr").ToList();
+            var serviceId = serviceObj[0].id;
+            //utworzenie uzytkownika Flickr
+            Flickr user = new Flickr();
+            //podmienic pozniej na credentials z bazy danych
+            var credentials = network.Credentials.Where<Credentials>(x => x.service_id == serviceId).ToList();
+            user.ApiKey = credentials[0].key;
+            user.ApiSecret = credentials[0].secret;
+            //pobieranie followersow uzytkownika z formularza
+            var flickrFollowers = user.ContactsGetPublicList(form.initialVertex);
+            //dodanie uzytkownika poczatkowego do tabeli vertex
+            IsUserIdInVertex(form.initialVertex, serviceObj[0].id);
+            //pobranie id uzytkonika poczatkowego w tabeli vertex
+            var userId = network.VertexDb.Where<VertexDb>(x => x.identifier == form.initialVertex && x.service_id == serviceId).ToList();
+            int numberOfPages = flickrFollowers.Pages;
+            //zapis pierwszej partii do bazy danych
+            foreach (var follower in flickrFollowers)
+            {
+                IsUserIdInVertex(follower.UserId, serviceObj[0].id);
+                var followerId = network.VertexDb.Where<VertexDb>(x => x.identifier == follower.UserId && x.service_id == serviceId).ToList();
+                var link = new LinkDb();
+                link.date_modified = data;
+                link.network_id = form.networkID;
+                link.source_id = followerId[0].id;
+                link.target_id = userId[0].id;
+                network.LinkDb.Add(link);
+            }
+            network.SaveChanges();
+            //zapis reszty danych
+            for (int i = 2; i <= numberOfPages; i++)
+            {
+                flickrFollowers = user.ContactsGetPublicList(form.initialVertex, i, 1000);
+                foreach (var follower in flickrFollowers)
+                {
+                    IsUserIdInVertex(follower.UserId, serviceObj[0].id);
+                    var followerId = network.VertexDb.Where<VertexDb>(x => x.identifier == follower.UserId && x.service_id == serviceId).ToList();
+                    var link = new LinkDb();
+                    link.date_modified = data;
+                    link.network_id = form.networkID;
+                    link.source_id = followerId[0].id;
+                    link.target_id = userId[0].id;
+                    network.LinkDb.Add(link);
+                }
+                network.SaveChanges();
+            }
             return Ok();
         }
         //POST: /api/FlickrFollowersAPI/FlickrNetwork
         [HttpPost]
         public IHttpActionResult FlickrNetwork([FromBody] FormFlickrFollowers form)
         {
+            //data rozpoczęcia pobierania
+            var data = DateTime.Now;
+            //liczba wykonanych zapytan
+            int queries = 0;
+            //dodac pobieranie id serwisu z bazy danych
+            var serviceObj = network.ServiceDb.Where<ServiceDb>(x => x.name == "Flickr").ToList();
+            var serviceId = serviceObj[0].id;
+            //utworzenie uzytkownika Flickr
+            Flickr user = new Flickr();
+            //podmienic pozniej na credentials z bazy danych
+            var credentials = network.Credentials.Where<Credentials>(x => x.service_id == serviceId).ToList();
+            user.ApiKey = credentials[0].key;
+            user.ApiSecret = credentials[0].secret;
+            //pobieranie followersow uzytkownika z formularza
+            var flickrFollowers = user.ContactsGetPublicList(form.initialVertex);
+            int numberOfPages = flickrFollowers.Pages;
+            queries++;
+            //dodanie uzytkownika poczatkowego do tabeli vertex
+            IsUserIdInVertex(form.initialVertex, serviceObj[0].id);
+            //pobranie id uzytkonika poczatkowego w tabeli vertex
+            var userId = network.VertexDb.Where<VertexDb>(x => x.identifier == form.initialVertex && x.service_id == serviceId).ToList();
+            //zapis pierwszej partii do bazy danych
+            foreach (var follower in flickrFollowers)
+            {
+                IsUserIdInVertex(follower.UserId, serviceObj[0].id, follower.UserName);
+                var followerId = network.VertexDb.Where<VertexDb>(x => x.identifier == follower.UserId && x.service_id == serviceId).ToList();
+                var link = new LinkDb();
+                link.date_modified = data;
+                link.network_id = form.networkID;
+                link.source_id = followerId[0].id;
+                link.target_id = userId[0].id;
+                network.LinkDb.Add(link);
+            }
+            network.SaveChanges();
+            //zapis reszty danych
+            for (int i = 2; i <= numberOfPages; i++)
+            {
+                flickrFollowers = user.ContactsGetPublicList(form.initialVertex, i, 1000);
+                foreach (var follower in flickrFollowers)
+                {
+                    IsUserIdInVertex(follower.UserId, serviceObj[0].id, follower.UserName);
+                    var followerId = network.VertexDb.Where<VertexDb>(x => x.identifier == follower.UserId && x.service_id == serviceId).ToList();
+                    var link = new LinkDb();
+                    link.date_modified = data;
+                    link.network_id = form.networkID;
+                    link.source_id = followerId[0].id;
+                    link.target_id = userId[0].id;
+                    network.LinkDb.Add(link);
+                }
+                network.SaveChanges();
+            }
+            //utworzenie listy followersow
+            var followersList = flickrFollowers.ToList();
+            //lista uzytych followersow
+            List<Contact> usedFollowers = new List<Contact>();
+            Contact initialUser = new Contact();
+            initialUser.UserId = form.initialVertex;
+            usedFollowers.Add(initialUser);
+            //usedFollowers.Add(form.initialVertex);
+            while (queries < form.numberOfQueries && followersList.Any())
+            {
+                queries++;
+                var newInitialVertex = followersList[0];
+                userId = network.VertexDb.Where<VertexDb>(x => x.identifier == newInitialVertex.UserId && x.service_id == serviceId).ToList();
+                flickrFollowers = user.ContactsGetPublicList(newInitialVertex.UserId);
+                usedFollowers.Add(newInitialVertex);
+                followersList = followersList.Union(flickrFollowers).Except(usedFollowers).ToList();
+                foreach (var follower in flickrFollowers)
+                {
+                    IsUserIdInVertex(follower.UserId, serviceObj[0].id, follower.UserName);
+                    var followerId = network.VertexDb.Where<VertexDb>(x => x.identifier == follower.UserId && x.service_id == serviceId).ToList();
+                    var link = new LinkDb();
+                    link.date_modified = data;
+                    link.network_id = form.networkID;
+                    link.source_id = followerId[0].id;
+                    link.target_id = userId[0].id;
+                    network.LinkDb.Add(link);
+                }
+                network.SaveChanges();
+
+                //zapis kolejnych partii danych
+                numberOfPages = flickrFollowers.Pages;
+                for (int i = 2; i <= numberOfPages; i++)
+                {
+                    flickrFollowers = user.ContactsGetPublicList(newInitialVertex.UserId, i, 1000);
+                    followersList = followersList.Union(flickrFollowers).Except(usedFollowers).ToList();
+                    foreach (var follower in flickrFollowers)
+                    {
+                        IsUserIdInVertex(follower.UserId, serviceObj[0].id, follower.UserName);
+                        var followerId = network.VertexDb.Where<VertexDb>(x => x.identifier == follower.UserId && x.service_id == serviceId).ToList();
+                        var link = new LinkDb();
+                        link.date_modified = data;
+                        link.network_id = form.networkID;
+                        link.source_id = followerId[0].id;
+                        link.target_id = userId[0].id;
+                        network.LinkDb.Add(link);
+                    }
+                    network.SaveChanges();
+                }
+
+            }
             return Ok();
         }
     }
--- SocNet/TwitterServices/Controllers/TwitterNetworkAPIController.cs
+++ SocNet/TwitterServices/Controllers/TwitterNetworkAPIController.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Tweetinvi;
 using TwitterServices.Models;
 
 namespace TwitterServices.Controllers
 {
     public class TwitterNetworkAPIController : ApiController
     {
+        //dodawanie nowych wierzchołków do bayz danych
+        public void IsUserIdInVertex(long VertexId, int serviceID, string VertexName = null)
+        {
+            string vertexIdStr = VertexId.ToString();
+            var existUser = network.VertexDb.Where<VertexDb>(x => x.identifier == vertexIdStr && x.service_id == serviceID).ToList();
+            if (existUser.Count == 0 && VertexName != null)
+            {
+                var vertex = new VertexDb();
+                vertex.identifier = vertexIdStr;
+                vertex.name = VertexName;
+                vertex.service_id = serviceID;
+                network.VertexDb.Add(vertex);
+                network.SaveChanges();
+            }
+            else if (existUser.Count == 0 && VertexName == null)
+            {
+                var vertex = new VertexDb();
+                vertex.identifier = vertexIdStr;
+                vertex.service_id = serviceID;
+                network.VertexDb.Add(vertex);
+                network.SaveChanges();
+            }
+        }
         //POST: /api/TwitterNetworkAPI/TwitterFollowers
         [HttpPost]
         public IHttpActionResult TwitterFollowers([FromBody] FormTwitterNetwork form)
@@ -38,6 +62,236 @@ namespace TwitterServices.Controllers
         [HttpPost]
         public IHttpActionResult TwitterNetwork([FromBody] FormTwitterNetwork form)
         {
+            //data utworzenia krawedzi
+            var data = DateTime.Now;
+            //ilosc zapytań
+            int queries = 0;
+            //akredytacja
+            var credentials = network.Credentials.Where<Credentials>(x => x.ServiceDb.name == "Twitter").ToList();
+            Auth.SetApplicationOnlyCredentials(credentials[0].key, credentials[0].secret, true);
+            //okiełznanie limitów twitter
+            RateLimit.RateLimitTrackerMode = RateLimitTrackerMode.TrackOnly;
+            TweetinviEvents.QueryBeforeExecute += (sender, args2) =>
+            {
+                var queryRateLimits = RateLimit.GetQueryRateLimit(args2.QueryURL);
+
+                // Some methods are not RateLimited. Invoking such a method will result in the queryRateLimits to be null
+                if (queryRateLimits != null)
+                {
+                    if (queryRateLimits.Remaining > 0)
+                    {
+                        // We have enough resource to execute the query
+                        return;
+                    }
+                    else
+                    {
+                        while (queryRateLimits.Remaining < 1)
+                        {
+                            foreach (var cred in network.Credentials.Where<Credentials>(x => x.ServiceDb.name == "Twitter").ToList())
+                            {
+                                var credential = Auth.SetApplicationOnlyCredentials(cred.key, cred.secret, true);
+                                queryRateLimits = RateLimit.GetQueryRateLimit(args2.QueryURL, credential);
+                                if (queryRateLimits.Remaining > 0)
+                                {
+                                    args2.TwitterQuery.TwitterCredentials = Auth.SetApplicationOnlyCredentials(cred.key, cred.secret, true);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            //wybor id serwisu
+            var serviceObj = network.ServiceDb.Where<ServiceDb>(x => x.name == "Twitter").ToList();
+            var serviceId = serviceObj[0].id;
+            //utworzenie uzytkownika poczatkowego twittera
+            var userName = Tweetinvi.User.GetUserFromId(form.initialVertex);
+            if (userName != null)
+            {
+                IsUserIdInVertex(form.initialVertex, serviceObj[0].id, userName.ScreenName);
+            }
+            else
+            {
+                IsUserIdInVertex(form.initialVertex, serviceObj[0].id);
+            }
+            var userId = network.VertexDb.Where<VertexDb>(x => x.identifier == form.initialVertex.ToString() && x.service_id == serviceId).ToList();
+            //tworzenie listy followersow i friendsow wierzcholka poczatkowego
+            var twitterFollowers = Tweetinvi.User.GetFollowerIds(form.initialVertex, form.queryLimit);
+            var twitterFriends = Tweetinvi.User.GetFriendIds(form.initialVertex, form.queryLimit);
+            queries++;
+            //zapis followersow dobazy danych
+            foreach (var follower in twitterFollowers)
+            {
+                //dodawnie nowego uzytkownika do wierzcholkow
+                var followerName = Tweetinvi.User.GetUserFromId(follower);
+                if (followerName != null)
+                {
+                    IsUserIdInVertex(follower, serviceObj[0].id, followerName.ScreenName);
+                }
+                else
+                {
+                    IsUserIdInVertex(follower, serviceObj[0].id);
+                }
+                //pobranie id wiersza bazy z wierzcholkami
+                var followerId = network.VertexDb.Where<VertexDb>(x => x.identifier == follower.ToString() && x.service_id == serviceId).ToList();
+                var link = new LinkDb();
+                link.date_modified = data;
+                link.network_id = form.networkID;
+                link.source_id = followerId[0].id;
+                link.target_id = userId[0].id;
+                network.LinkDb.Add(link);
+            }
+            network.SaveChanges();
+            //zapis friendsow do bazy danych
+            foreach (var friend in twitterFriends)
+            {
+                //dodawnie nowego uzytkownika do wierzcholkow
+                var followerName = Tweetinvi.User.GetUserFromId(friend);
+                if (followerName != null)
+                {
+                    IsUserIdInVertex(friend, serviceObj[0].id, followerName.ScreenName);
+                }
+                else
+                {
+                    IsUserIdInVertex(friend, serviceObj[0].id);
+                }
+                //pobranie id wiersza b azy z wierzcholkami
+                var friendId = network.VertexDb.Where<VertexDb>(x => x.identifier == friend.ToString() && x.service_id == serviceId).ToList();
+                var link = new LinkDb();
+                link.date_modified = data;
+                link.network_id = form.networkID;
+                link.source_id = userId[0].id;
+                link.target_id = friendId[0].id;
+                network.LinkDb.Add(link);
+            }
+            network.SaveChanges();
+            //utworzenie list uzytkownikow
+            //List<long> usersTwitter = twitterFollowers.Union(twitterFriends).ToList();
+            List<long> twitterFollowersList = twitterFollowers.ToList();
+            List<long> twitterFriendsList = twitterFriends.ToList();
+            List<long> usedUsers = new List<long>();
+            usedUsers.Add(form.initialVertex);
+            //rozpoczynamy pobieranie dla kolejnych wierzcholkow
+            while (queries < form.numberOfQueries && (twitterFriendsList.Any() || twitterFollowersList.Any()))
+            {
+                var rand = new Random();
+                bool bool1 = (twitterFriendsList.Any()) ? true : false;
+                bool bool2 = (twitterFollowersList.Any()) ? true : false;
+                if (rand.Next(100) % 2 == 1 && bool1 == true)
+                {
+                    queries++;
+                    var newInitialVertexId = twitterFriendsList[0];
+                    userId = network.VertexDb.Where<VertexDb>(x => x.identifier == newInitialVertexId.ToString() && x.service_id == serviceId).ToList();
+                    usedUsers.Add(newInitialVertexId);
+                    var newUsersFriends = Tweetinvi.User.GetFriendIds(newInitialVertexId, form.queryLimit);
+                    var newUsersFollowers = Tweetinvi.User.GetFollowerIds(newInitialVertexId, form.queryLimit);
+                    twitterFriendsList = twitterFriendsList.Union(newUsersFriends).Except(usedUsers).ToList();
+                    twitterFollowersList = twitterFollowersList.Union(newUsersFollowers).Except(usedUsers).ToList();
+                    //zapis followersow dobazy danych
+                    foreach (var follower in newUsersFollowers)
+                    {
+                        //dodawnie nowego uzytkownika do wierzcholkow
+                        var followerName = Tweetinvi.User.GetUserFromId(follower);
+                        if (followerName != null)
+                        {
+                            IsUserIdInVertex(follower, serviceObj[0].id, followerName.ScreenName);
+                        }
+                        else
+                        {
+                            IsUserIdInVertex(follower, serviceObj[0].id);
+                        }
+                        //pobranie id wiersza b azy z wierzcholkami
+                        var followerId = network.VertexDb.Where<VertexDb>(x => x.identifier == follower.ToString() && x.service_id == serviceId).ToList();
+                        var link = new LinkDb();
+                        link.date_modified = data;
+                        link.network_id = form.networkID;
+                        link.source_id = followerId[0].id;
+                        link.target_id = userId[0].id;
+                        network.LinkDb.Add(link);
+                    }
+                    network.SaveChanges();
+                    //zapis friendsow do bazy danych
+                    foreach (var friend in newUsersFriends)
+                    {
+                        //dodawnie nowego uzytkownika do wierzcholkow
+                        var followerName = Tweetinvi.User.GetUserFromId(friend);
+                        if (followerName != null)
+                        {
+                            IsUserIdInVertex(friend, serviceObj[0].id, followerName.ScreenName);
+                        }
+                        else
+                        {
+                            IsUserIdInVertex(friend, serviceObj[0].id);
+                        }
+                        //pobranie id wiersza b azy z wierzcholkami
+                        var friendId = network.VertexDb.Where<VertexDb>(x => x.identifier == friend.ToString() && x.service_id == serviceId).ToList();
+                        var link = new LinkDb();
+                        link.date_modified = data;
+                        link.network_id = form.networkID;
+                        link.source_id = userId[0].id;
+                        link.target_id = friendId[0].id;
+                        network.LinkDb.Add(link);
+                    }
+                    network.SaveChanges();
+                }
+                else if (rand.Next(100) % 2 == 0 && bool2 == true)
+                {
+                    queries++;
+                    var newInitialVertexId = twitterFollowersList[0];
+                    userId = network.VertexDb.Where<VertexDb>(x => x.identifier == newInitialVertexId.ToString() && x.service_id == serviceId).ToList();
+                    usedUsers.Add(newInitialVertexId);
+                    var newUsersFriends = Tweetinvi.User.GetFriendIds(newInitialVertexId, form.queryLimit);
+                    var newUsersFollowers = Tweetinvi.User.GetFollowerIds(newInitialVertexId, form.queryLimit);
+                    twitterFriendsList = twitterFriendsList.Union(newUsersFriends).Except(usedUsers).ToList();
+                    twitterFollowersList = twitterFollowersList.Union(newUsersFollowers).Except(usedUsers).ToList();
+                    //zapis followersow dobazy danych
+                    foreach (var follower in newUsersFollowers)
+                    {
+                        //dodawnie nowego uzytkownika do wierzcholkow
+                        var followerName = Tweetinvi.User.GetUserFromId(follower);
+                        if (followerName != null)
+                        {
+                            IsUserIdInVertex(follower, serviceObj[0].id, followerName.ScreenName);
+                        }
+                        else
+                        {
+                            IsUserIdInVertex(follower, serviceObj[0].id);
+                        }
+                        //pobranie id wiersza b azy z wierzcholkami
+                        var followerId = network.VertexDb.Where<VertexDb>(x => x.identifier == follower.ToString() && x.service_id == serviceId).ToList();
+                        var link = new LinkDb();
+                        link.date_modified = data;
+                        link.network_id = form.networkID;
+                        link.source_id = followerId[0].id;
+                        link.target_id = userId[0].id;
+                        network.LinkDb.Add(link);
+                    }
+                    network.SaveChanges();
+                    //zapis friendsow do bazy danych
+                    foreach (var friend in newUsersFriends)
+                    {
+                        //dodawnie nowego uzytkownika do wierzcholkow
+                        var followerName = Tweetinvi.User.GetUserFromId(friend);
+                        if (followerName != null)
+                        {
+                            IsUserIdInVertex(friend, serviceObj[0].id, followerName.ScreenName);
+                        }
+                        else
+                        {
+                            IsUserIdInVertex(friend, serviceObj[0].id);
+                        }
+                        //pobranie id wiersza b azy z wierzcholkami
+                        var friendId = network.VertexDb.Where<VertexDb>(x => x.identifier == friend.ToString() && x.service_id == serviceId).ToList();
+                        var link = new LinkDb();
+                        link.date_modified = data;
+                        link.network_id = form.networkID;
+                        link.source_id = userId[0].id;
+                        link.target_id = friendId[0].id;
+                        network.LinkDb.Add(link);
+                    }
+                    network.SaveChanges();
+                }
+            }
             return Ok();
         }
 
