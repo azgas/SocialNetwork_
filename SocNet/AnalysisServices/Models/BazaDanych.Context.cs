ef50fc2af588a009654b38925b2f53947c87136b
azgas <azgaslik@gmail.com>
2016-10-23 19:48:43 +0200

    usunięcie projektu (wywaliłam nie te pliki, szybciej będzie zrobić nowy...)

--- SocNet/AnalysisServices/Models/BazaDanych.Context.cs
+++ SocNet/AnalysisServices/Models/BazaDanych.Context.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AnalysisServices.Models
+namespace AnalysisServices2.Models
 {
     using System;
     using System.Data.Entity;
